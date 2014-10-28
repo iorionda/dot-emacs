52b8f22238b49acf9c769deb0ef52df8cea64dbd
Iori ONDA <iori.onda@gmail.com>
2014-10-28 18:43:00 +0900

    Initial commit

---
+++ init_loader/09_paren.el
@@ -0,0 +1,21 @@
+;; paren
+(show-paren-mode t)
+(custom-set-variables
+ '(show-paren-delay 0)
+ '(show-paren-style 'expressin)
+ '(parens-require-spaces nil))
+
+(set-face-attribute 'show-paren-match-face nil
+                    :background nil :foreground nil
+                    :underline "#ffff00" :weight 'extra-bold)
+;; smartparen
+(smartparens-global-mode t)
+(sp-local-pair 'minibuffer-inactive-mode "'" nil :actions nil)
+
+(with-eval-after-load "ruby-mode"
+  (require 'smartparens-ruby)
+  (set-face-attribute 'sp-show-pair-match-face nil
+                      :background "grey20" :foreground "green"
+                      :weight 'semi-bold))
+
+(add-hook 'ruby-mode-hook 'show-smartparens-mode)
