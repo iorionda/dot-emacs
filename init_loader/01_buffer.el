6aa1cf448a639340cdf2cee361302a2c5ed05775
Iori ONDA <iori.onda@gmail.com>
2014-10-28 18:23:48 +0900

    Flamboyanty

--- init_loader/01_buffer.el
+++ init_loader/01_buffer.el
@@ -22,10 +22,9 @@
 (setq hl-line-face 'hlline-face)
 (global-hl-line-mode)
 
-
 ;; auto-save
 (defun my/auto-save-buffers ()
-  (save-excurison
+  (save-excursion
    (dolist (buffer (buffer-list))
      (set-buffer buffer)
      (when (and (buffer-file-name)
@@ -37,6 +36,35 @@
 (run-with-idle-timer 10 t 'my/auto-save-buffers)
 
 ;; naming of same name file
-(require 'uniquify)
 (custom-set-variables
  '(uniquify-buffer-name-style 'post-forward-angle-brackets))
+
+;; raibow-delimiters-mode
+(require 'rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+(custom-set-faces '(rainbow-delimiters-depth-1-face ((t (:foreground "#FF8F22")))))
+(cl-loop
+ for index from 1 to rainbow-delimiters-max-face-count
+ do
+ (let ((face (intern (format "rainbow-delimiters-depth-%d-face" index))))
+   (cl-callf color-saturate-name (face-foreground face) 30)))
+(rainbow-delimiters-mode t)
+
+;; git-gutter
+(global-git-gutter-mode t)
+(define-key global-map (kbd "C-x g") 'git-gutter:toggle)
+(setq git-gutter:window-width 1)
+
+(setq git-gutter:modified-sign " ")
+(setq git-gutter:added-sign "+")
+(setq git-gutter:deleted-sign "-")
+
+(set-face-foreground 'git-gutter:modified "blue")
+(set-face-foreground 'git-gutter:added "green")
+(set-face-foreground 'git-gutter:deleted "red")
+
+;; highlight-indentation
+(highlight-indentation-mode t)
+(set-face-background 'highlight-indentation-face "midnight blue")
+(set-face-background 'highlight-indentation-current-column-face "red")
+(setq highlight-indentation-offset 4)
