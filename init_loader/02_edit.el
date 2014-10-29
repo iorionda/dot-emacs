7ba02b6e3f3dd715979b44581a5f0cf4019543d8
Iori ONDA <iori.onda@gmail.com>
2014-10-29 14:20:49 +0900

    Remove autopair settings

--- init_loader/02_edit.el
+++ init_loader/02_edit.el
@@ -11,22 +11,3 @@
 ;; electrict-mode
 (custom-set-variables
  '(electric-indent-mode nil))
-
-;; autopair
-(custom-set-variables
- '(autopair-blink nil)
- '(autopair-blink-delay 0))
-
-(defvar my/autopair-enabled-modes
-  '(c-mode
-    c++-mode
-    java-mode
-    python-mode
-    ruby-mode
-    sh-mode
-    js-mode
-    coffee-mode
-    cperl-mode))
-
-(dolist (mode my/autopair-enabled-modes)
-  (add-hook (intern (format "%s-hook" mode)) 'autopair-mode))
